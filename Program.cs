cdd223c3440dd2b705bc3ba849558b3154cf5834
bycher <mikhbych@gmail.com>
2024-12-05 11:07:42 +0500

    Add exception handling in ConfigParser

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text.Json;
 using Serilog;
+using Serilog.Events;
 using Simulation.Models;
 using Simulation.Services;
 
@@ -10,20 +11,22 @@ internal class Program
     private static readonly string LogsFilePath = Path.Combine(
         "logs", $"log_{DateTime.Now:yyyyMMdd_HHmmss}.txt");
 
-    private static readonly string LogsTemplate = 
+    private static readonly string LogsFileTemplate = 
         "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
 
     public static void Main()
     {
-        var configParser = new ConfigParser("default.json");
+        var logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Error)
+            .WriteTo.File(LogsFilePath, outputTemplate: LogsFileTemplate)
+            .CreateLogger();
+
+        var configParser = new ConfigParser("default.json", logger);
         if (configParser.TryParse(out var simulationOptions))
         {
             var simulation = new Simulation.Models.Simulation(
-                simulationOptions,
-                new ConsoleMapRenderer(),
-                new LoggerConfiguration()
-                    .WriteTo.File(LogsFilePath, outputTemplate: LogsTemplate)
-                    .CreateLogger());
+                simulationOptions, new ConsoleMapRenderer(), logger);
 
             simulation.Start();
             HandleUserInput(simulation);
--- Services/ConfigParser.cs
+++ Services/ConfigParser.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using System.Text.Json;
 using Serilog;
 using Simulation.Models;
@@ -5,30 +6,49 @@ using Simulation.Services.Interfaces;
 
 namespace Simulation.Services;
 
-public class ConfigParser(string fileName) : IConfigParser
+public class ConfigParser(string fileName, ILogger logger) : IConfigParser
 {
+    private static readonly HashSet<string> ValidPropertyNames = new(typeof(SimulationOptions)
+        .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+        .Select(p => p.Name));
+
     private readonly string _path = Path.Combine("config", fileName);
+    private readonly ILogger _logger = logger;
 
     public bool TryParse(out SimulationOptions? simulationOptions)
     {
         simulationOptions = null;
         if (!File.Exists(_path))
         {
-            Console.WriteLine("Error: Config file doesn't exist!");
+            _logger.Error("Config file doesn't exist!");
             return false;
         }
         
-        using var reader = new StreamReader(_path);
-        var json = reader.ReadToEnd();
         try
         {
+            var json = File.ReadAllText(_path);
+            ValidateJson(json);
             simulationOptions = JsonSerializer.Deserialize<SimulationOptions>(json);
         }
         catch (Exception e)
         {
-            Console.WriteLine("Error: JSON deserialization is failed! " + e);
+            _logger.Error($"Config parsing is failed! {e.Message}");
             return false;
         }
+
+        _logger.Information("Config file parsed successfully!");
+        return true;
+    }
+
+    private static bool ValidateJson(string json)
+    {
+        var document = JsonDocument.Parse(json);
+
+        foreach (var property in document.RootElement.EnumerateObject())
+        {
+            if (!ValidPropertyNames.Contains(property.Name))
+                throw new JsonException($"Unknown property: \"{property.Name}\"");
+        } 
         return true;
     }
 }
